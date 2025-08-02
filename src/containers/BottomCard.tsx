e7fb15a155200016fcc2a5af2fb31af0a3ea1d54
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-08-02 16:57:45 +0100

    Use StyledLink instead for mypage GitHub link

--- src/containers/BottomCard.tsx
+++ src/containers/BottomCard.tsx
@@ -1,22 +1,25 @@
 import type { FC } from 'react'
+import { styled } from '@mui/material'
+import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
-import { SocialIcon } from 'react-social-icons'
 import SocialStack from '../components/SocialStack'
 import StyledCard from '../components/StyledCard'
 import type { TopCardProps } from './TopCard'
+
+const StyledLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  textDecorationColor: theme.palette.primary.contrastText,
+}))
 
 const BottomCard: FC<TopCardProps> = ({ content }) => (
   <StyledCard>
     <Typography>{content.name}</Typography>
     <SocialStack socials={content.socials} />
-    <Typography>
-      Created with mypage {import.meta.env.VITE_VERSION}
-      <SocialIcon
-        style={{ height: '1.5em', width: '1.5em' }}
-        target="_blank"
-        url="https://github.com/Grvs44/mypage"
-      />
-    </Typography>
+    Created with{' '}
+    <StyledLink target="_blank" href="https://github.com/Grvs44/mypage">
+      mypage
+    </StyledLink>{' '}
+    {import.meta.env.VITE_VERSION}
   </StyledCard>
 )
 
