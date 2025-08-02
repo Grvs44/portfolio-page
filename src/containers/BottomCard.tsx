9d3e5cb95b442895a83ee75eabd7311100bebf6f
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-08-02 16:40:04 +0100

    Add mypage GitHub SocialIcon

--- src/containers/BottomCard.tsx
+++ src/containers/BottomCard.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 import Typography from '@mui/material/Typography'
+import { SocialIcon } from 'react-social-icons'
 import SocialStack from '../components/SocialStack'
 import StyledCard from '../components/StyledCard'
 import type { TopCardProps } from './TopCard'
@@ -8,6 +9,14 @@ const BottomCard: FC<TopCardProps> = ({ content }) => (
   <StyledCard>
     <Typography>{content.name}</Typography>
     <SocialStack socials={content.socials} />
+    <Typography>
+      Created with mypage {import.meta.env.VITE_VERSION}
+      <SocialIcon
+        style={{ height: '1.5em', width: '1.5em' }}
+        target="_blank"
+        url="https://github.com/Grvs44/mypage"
+      />
+    </Typography>
   </StyledCard>
 )
 
