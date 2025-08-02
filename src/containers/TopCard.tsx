8dee4114cd02065fdf54fe2abb6dd112146d9154
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-08-02 12:47:40 +0100

    Show description

--- src/containers/TopCard.tsx
+++ src/containers/TopCard.tsx
@@ -1,23 +1,24 @@
 import type { FC } from 'react'
 import Card from '@mui/material/Card'
-import Skeleton from '@mui/material/Skeleton'
 import Typography from '@mui/material/Typography'
 import SocialStack from '../components/SocialStack'
-import type { Social } from '../types'
+import type { TopContent } from '../types'
 
 export type TopCardProps = {
-  content?: {
-    name: string
-    socials?: Social[]
-  }
+  content: TopContent
 }
 
 const TopCard: FC<TopCardProps> = ({ content }) => (
   <Card>
-    <Typography component="h1">
-      {content ? content.name : <Skeleton />}
+    <Typography component="h1" variant="h2">
+      {content.name}
     </Typography>
     <SocialStack socials={content?.socials} />
+    {content.description ? (
+      <Typography sx={{ whiteSpace: 'pre-wrap' }}>
+        {content.description}
+      </Typography>
+    ) : null}
   </Card>
 )
 
