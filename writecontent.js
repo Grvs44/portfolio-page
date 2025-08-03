c5205af56f41a2855f401ef1b18f51899cbe0da1
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-08-03 17:28:23 +0100

    Write content in content.js and output JSON and VITE_NAME

---
+++ writecontent.js
@@ -0,0 +1,12 @@
+import { writeFile } from 'node:fs/promises'
+
+export default async function run() {
+  const content = (await import('./content.js')).default
+  for (const key of ['name', 'description', 'about']) {
+    content[key] = content[key].trim()
+  }
+  await writeFile('./src/content/content.json', JSON.stringify(content))
+  await writeFile('./.env.local', 'VITE_NAME=' + content.name)
+}
+
+run()
