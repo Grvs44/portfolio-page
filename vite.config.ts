e85783a0f69022d8d7c9e31b79365efca9aa4327
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-08-03 11:47:31 +0100

    Add webmanifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,38 @@
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 import license from 'rollup-plugin-license'
 import { defineConfig, type Plugin } from 'vite'
 import jsconfigPaths from 'vite-jsconfig-paths'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const env = dotenv.config().parsed
+if (!env) throw '.env could not be parsed'
+
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     jsconfigPaths(),
     VitePWA({
       injectRegister: 'inline',
-      manifest: false,
+      manifest: {
+        short_name: env.VITE_NAME,
+        name: env.VITE_NAME,
+        id: '/',
+        icons: [
+          {
+            src: 'img.png',
+            type: 'image/png',
+            sizes: '240x240',
+          },
+        ],
+        start_url: '.',
+        scope: '.',
+        display_override: ['window-controls-overlay', 'standalone'],
+        theme_color: '#1976d2',
+        background_color: '#ffffff',
+      },
       workbox: { clientsClaim: true, skipWaiting: true },
     }),
     createHtmlPlugin({ minify: true }),
@@ -22,7 +43,6 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          c: ['src/content'],
           e: ['@emotion/react', '@emotion/styled'],
           m: ['@mui/material'],
           r: ['react', 'react-dom'],
