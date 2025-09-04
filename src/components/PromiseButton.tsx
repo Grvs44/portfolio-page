f5222ab41a3a0861e59cf2d1e82875e83382d5a6
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-09-04 17:54:27 +0100

    Fix icon colours in Top Card Fixes #24

--- src/components/PromiseButton.tsx
+++ src/components/PromiseButton.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from 'react'
 import CancelIcon from '@mui/icons-material/Cancel'
 import DoneIcon from '@mui/icons-material/Done'
+import { styled } from '@mui/material'
 import IconButton, { IconButtonProps } from '@mui/material/IconButton'
 
 enum State {
@@ -13,6 +14,10 @@ export type PromiseButtonProps = IconButtonProps & {
   getPromise: () => Promise<void>
   title: string
 }
+
+const Button = styled(IconButton)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+}))
 
 const PromiseButton: FC<PromiseButtonProps> = (props) => {
   const [state, setState] = useState<State>(State.Ready)
@@ -44,7 +49,7 @@ const PromiseButton: FC<PromiseButtonProps> = (props) => {
   }, [state])
 
   return (
-    <IconButton aria-label={title} title={title} onClick={share}>
+    <Button {...props} aria-label={title} onClick={share}>
       {state == State.Ready ? (
         props.children
       ) : state == State.Success ? (
@@ -52,7 +57,7 @@ const PromiseButton: FC<PromiseButtonProps> = (props) => {
       ) : (
         <CancelIcon />
       )}
-    </IconButton>
+    </Button>
   )
 }
 
--- src/components/TopIconButton.tsx
+++ src/components/TopIconButton.tsx
@@ -2,8 +2,9 @@ import { styled } from '@mui/material'
 import IconButton from '@mui/material/IconButton'
 
 // Adapted from https://mui.com/material-ui/react-dialog/#customization
-export default styled(IconButton)({
+export default styled(IconButton)(({ theme }) => ({
   position: 'absolute',
   right: 8,
   top: 8,
-})
+  color: theme.palette.primary.contrastText,
+}))
