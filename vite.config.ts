1afc4ca7d7a93fad4500c34324b02267a4bc6a65
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-09-26 21:37:58 +0100

    Make includeAssets relative to ./public/ to cache images correctly Fixes #48

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     react(),
     jsconfigPaths(),
     VitePWA({
-      includeAssets: ['public/*.jpg', 'public/*.jpeg', 'public/*.png'],
+      includeAssets: ['*.jpg', '*.jpeg', '*.png'],
       manifest: {
         short_name: env.VITE_NAME,
         name: env.VITE_NAME,
